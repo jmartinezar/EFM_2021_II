298d6f4deef86ac9111a14cfebc0ac35713f8014
jmartinezar <jmartinezar@unal.edu.co>
2021-10-30 22:29:50 +0000

    Cambio de las constantes para poder variar fácilmente la longitud de onda y las temperaturas

--- Err_aprox.cpp
+++ Err_aprox.cpp
@@ -3,14 +3,19 @@
 
 typedef double REAL;
 
-const REAL C = 1.5e04; // constante (hc)/(lk) para lambda =960nm
+const REAL C_1 = 1.44e-02; // constante (hc)/(lk) para lambda =960nm
+const REAL L = 960e-09; //longitud de onda
+const REAL C = C_1/L;
+const REAL T_max = 2596.5; //temperatura máxima de referencia
+const REAL Tp_1 = 2567; //temperaturas menores que T_max para hacer el cálculo del error
+const REAL Tp_2 = 587;
 
 REAL R(REAL T_1, REAL T_2); // declaración de la función calculada restando el 1 arriba y abajo
 REAL A(REAL T_1, REAL T_2); // declaración de la función sin contar ese 1
 
 int main(void)
 {
-		std::cout << std::fabs(1-A(2596.5, 2567)/R(2596.5, 2567)) << "\t" << std::fabs(1-A(2596.5, 587)/R(2596.5, 587)) << "\n";
+		std::cout << std::fabs(1-A(T_max, Tp_1)/R(T_max, Tp_1)) << "\t" << std::fabs(1-A(T_max, Tp_2)/R(T_max, Tp_2)) << "\n";
     //imprime en pantalla el error relativo usando primero dos temperaturas altas y despues una alta y una baja
 	return 0;
 }
