93661f9df8765e5acc743f8d90e1087cb884bb80
jmartinezar <jmartinezar@unal.edu.co>
2021-10-30 22:11:07 +0000

    Adición de comentario que facilitan la lectura del código

--- Err_aprox.cpp
+++ Err_aprox.cpp
@@ -3,14 +3,15 @@
 
 typedef double REAL;
 
-const REAL C = 1.5e04;
+const REAL C = 1.5e04; // constante (hc)/(lk) para lambda =960nm
 
-REAL R(REAL T_1, REAL T_2);
-REAL A(REAL T_1, REAL T_2);
+REAL R(REAL T_1, REAL T_2); // declaración de la función calculada restando el 1 arriba y abajo
+REAL A(REAL T_1, REAL T_2); // declaración de la función sin contar ese 1
 
 int main(void)
 {
 		std::cout << std::fabs(1-A(2596.5, 2567)/R(2596.5, 2567)) << "\t" << std::fabs(1-A(2596.5, 587)/R(2596.5, 587)) << "\n";
+    //imprime en pantalla el error relativo usando primero dos temperaturas altas y despues una alta y una baja
 	return 0;
 }
 
